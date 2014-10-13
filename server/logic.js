7f57e006a97693c4a8ac7b3f775ef9420e1e6a43
Robert Corey <rcorey@ziji-media.com>
2014-10-13 12:51:27 -0400

    seperate server and ai, delete old certs

--- server/logic.js
+++ server/logic.js
@@ -1,11 +1,98 @@
 var pokerEvaluator = require('poker-evaluator');
 var _ = require('lodash');
+var colors = require('colors');
 
 var ShotCaller = function (pHand, oHand, deck, pCard) {
   this.pHands = pHand;
   this.oHands = oHand;
   this.deck = deck;
   this.pCard = pCard;
+  this.turn = _.min(this.pHands, function (item) {return item.length;}).length;
+};
+
+ShotCaller.prototype.printCard = function(card) {
+  var str = card + ' ';
+  switch (card[1]) {
+    case 'h':
+      return str.red;
+    case 'd':
+      return str.blue;
+    case 'c':
+      return str.green;
+    case 's':
+      return str.black;
+  }
+};
+
+ShotCaller.prototype.printHands = function (hands, title) {
+  console.log('\n' + title);
+  console.log('-------------------------------------------------------------');
+  for (var i = 0; i < 5; i++) {
+    var row = '';
+    for (var j = 0; j < hands.length; j++) {
+      if (hands[j][i]) {
+        row += this.printCard(hands[j][i]);
+      } else {
+        row += 'xx '.yellow;
+      }
+    }
+    console.log(row);
+  }
+};
+//Todo print missed ones, sorting fucks up on paint cards
+ShotCaller.prototype.printDeck = function () {
+  console.log('Remaining Cards: ');
+  console.log('-------------------------------------------------------------');
+  var sortedDeck = this.deck.sort(function (a, b) {
+    if (a[1] > b[1]) {
+      return 1;
+    } else if (a[1] < b[1]) {
+      return -1;
+    } else if (a === 'a') {
+      return 1;
+    } else if (b === 'a') {
+      return -1;
+    } else if (a === 'k') {
+      return 1;
+    } else if (b === 'k') {
+      return -1;
+    } else if (a === 'q') {
+      return 1;
+    } else if (b === 'q') {
+      return -1;
+    } else if (a === 'j') {
+      return 1;
+    } else if (b === 'j') {
+      return -1;
+    } else if (a === 'T') {
+      return 1;
+    } else if (b === 'T') {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  var currentSuit = 'c',
+  row = '',
+  that = this;
+  _.forEach(this.deck, function (card) {
+    if (currentSuit !== card[1]) {
+      console.log(row);
+      row = '';
+      currentSuit = card[1];
+    }
+    row += that.printCard(card);
+  });
+  console.log(row);
+};
+
+ShotCaller.prototype.printModel = function() {
+  this.printHands(this.oHands, 'Villian Hands');
+  this.printHands(this.pHands, 'Player Hands');
+  console.log('Player Card: ', this.printCard(this.pCard));
+  this.printDeck(); 
 };
 
 ShotCaller.prototype._isFirstWinner = function (aHand, bHand) {
@@ -29,6 +116,7 @@ ShotCaller.prototype._simulateRounds = function (pHandOriginal, oHandOriginal, u
   if (useCard) {
     pHandOriginal.push(this.pCard);
   }
+
   for (var i = 0; i < NUM_ROUNDS; i++) {
     var pHand = pHandOriginal.slice(0);
     var oHand = oHandOriginal.slice(0);
@@ -43,19 +131,38 @@ ShotCaller.prototype._simulateRounds = function (pHandOriginal, oHandOriginal, u
     if (this._isFirstWinner(pHand, oHand)) {
       playerWin += 1;
     }
+    pHand = null;
+    oHand = null;
+    deck = null;
   }
+
   if (useCard) {
     pHandOriginal.pop();
   }
   return Math.round( (playerWin / NUM_ROUNDS) * 100);
 };
 
+ShotCaller.prototype.cleanOHands = function () {
+  _.forEach(this.oHands, function (item) {
+    if (item.length === 5) {
+      item.splice(4, 1);
+    }
+  });
+};
+
 ShotCaller.prototype.callShot = function (verbose) {
+  if (this.turn === 4) {
+    this.cleanOHands();
+  }
+  verbose = true;
+  if (verbose) { this.printModel(); }
+
   var matchups =  [];
   var matchupsWithCard = [];
   var max, maxPos;
   for (var i = 0; i < this.pHands.length; i++) {
-    if (!this.pHands[i]) { break; }
+    // if the hand has already been given a card this turn continue
+    if (this.pHands[i].length > this.turn) {continue;}
     var withCard = this._simulateRounds(this.pHands[i], this.oHands[i], true);
     var noCard = this._simulateRounds(this.pHands[i], this.oHands[i]);
     var diff = withCard - noCard;
@@ -72,12 +179,8 @@ ShotCaller.prototype.callShot = function (verbose) {
       maxPos = i;
     }
   }
+  console.log('max difference is at', maxPos);
   return maxPos;
 };
 
-var ai = new ShotCaller(
-  model.playerHands,
-  model.opponentHands,
-  model.deck,
-  model.playerCard
-);
+module.exports = ShotCaller;
--- server/server.js
+++ server/server.js
@@ -1,204 +1,18 @@
-var pokerEvaluator = require('poker-evaluator');
-var _ = require('lodash');
-var colors = require('colors');
-
-var ShotCaller = function (pHand, oHand, deck, pCard) {
-  this.pHands = pHand;
-  this.oHands = oHand;
-  this.deck = deck;
-  this.pCard = pCard;
-  this.turn = _.min(this.pHands, function (item) {return item.length;}).length;
-};
-
-ShotCaller.prototype.printCard = function(card) {
-  var str = card + ' ';
-  switch (card[1]) {
-    case 'h':
-      return str.red;
-    case 'd':
-      return str.blue;
-    case 'c':
-      return str.green;
-    case 's':
-      return str.black;
-  }
-};
-
-ShotCaller.prototype.printHands = function (hands, title) {
-  console.log('\n' + title);
-  console.log('-------------------------------------------------------------');
-  for (var i = 0; i < 5; i++) {
-    var row = '';
-    for (var j = 0; j < hands.length; j++) {
-      if (hands[j][i]) {
-        row += this.printCard(hands[j][i]);
-      } else {
-        row += 'xx '.yellow;
-      }
-    }
-    console.log(row);
-  }
-};
-//Todo print missed ones, sorting fucks up on paint cards
-ShotCaller.prototype.printDeck = function () {
-  console.log('Remaining Cards: ');
-  console.log('-------------------------------------------------------------');
-  var sortedDeck = this.deck.sort(function (a, b) {
-    if (a[1] > b[1]) {
-      return 1;
-    } else if (a[1] < b[1]) {
-      return -1;
-    } else if (a === 'a') {
-      return 1;
-    } else if (b === 'a') {
-      return -1;
-    } else if (a === 'k') {
-      return 1;
-    } else if (b === 'k') {
-      return -1;
-    } else if (a === 'q') {
-      return 1;
-    } else if (b === 'q') {
-      return -1;
-    } else if (a === 'j') {
-      return 1;
-    } else if (b === 'j') {
-      return -1;
-    } else if (a === 'T') {
-      return 1;
-    } else if (b === 'T') {
-      return -1;
-    } else if (a > b) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-  var currentSuit = 'c',
-  row = '',
-  that = this;
-  _.forEach(this.deck, function (card) {
-    if (currentSuit !== card[1]) {
-      console.log(row);
-      row = '';
-      currentSuit = card[1];
-    }
-    row += that.printCard(card);
-  });
-  console.log(row);
-};
-
-ShotCaller.prototype.printModel = function() {
-  this.printHands(this.oHands, 'Villian Hands');
-  this.printHands(this.pHands, 'Player Hands');
-  console.log('Player Card: ', this.printCard(this.pCard));
-  this.printDeck(); 
-};
-
-ShotCaller.prototype._isFirstWinner = function (aHand, bHand) {
-  var aHandEval = pokerEvaluator.evalHand(aHand);
-  var bHandEval = pokerEvaluator.evalHand(bHand);
-  if (aHandEval.handtype > bHandEval.handtype) {
-    return true;
-  } else if (aHandEval.handtype < bHandEval.handtype) {
-    return false;
-  } else if (aHandEval.handRank > bHandEval.handRank) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-ShotCaller.prototype._simulateRounds = function (pHandOriginal, oHandOriginal, useCard) {
-  var NUM_ROUNDS = 10000;
-  var playerWin = 0;
-  //todo push / pop seems shitty 
-  if (useCard) {
-    pHandOriginal.push(this.pCard);
-  }
-
-  for (var i = 0; i < NUM_ROUNDS; i++) {
-    var pHand = pHandOriginal.slice(0);
-    var oHand = oHandOriginal.slice(0);
-    var deck = _.shuffle(this.deck.slice(0));
-    while (pHand.length < 5) {
-      pHand.push(deck.pop());
-    }
-    while(oHand.length < 5) {
-      oHand.push(deck.pop());
-    }
-
-    if (this._isFirstWinner(pHand, oHand)) {
-      playerWin += 1;
-    }
-    pHand = null;
-    oHand = null;
-    deck = null;
-  }
-
-  if (useCard) {
-    pHandOriginal.pop();
-  }
-  return Math.round( (playerWin / NUM_ROUNDS) * 100);
-};
-
-ShotCaller.prototype.cleanOHands = function () {
-  _.forEach(this.oHands, function (item) {
-    if (item.length === 5) {
-      item.splice(4, 1);
-    }
-  });
-};
-
-ShotCaller.prototype.callShot = function (verbose) {
-  if (this.turn === 4) {
-    this.cleanOHands();
-  }
-  verbose = true;
-  if (verbose) { this.printModel(); }
-
-  var matchups =  [];
-  var matchupsWithCard = [];
-  var max, maxPos;
-  for (var i = 0; i < this.pHands.length; i++) {
-    // if the hand has already been given a card this turn continue
-    if (this.pHands[i].length > this.turn) {continue;}
-    var withCard = this._simulateRounds(this.pHands[i], this.oHands[i], true);
-    var noCard = this._simulateRounds(this.pHands[i], this.oHands[i]);
-    var diff = withCard - noCard;
-    if (verbose) {
-      console.log(i , 'Player Hand: ', this.pHands[i], 'Opp Hand: ', this.oHands[i]);
-      console.log('Player Card: ', this.pCard);
-      console.log('With Card: ', withCard, '%');
-      console.log('Without Card: ', noCard, '%');
-      console.log('Difference: ', diff);
-      console.log('--------------------------------------------------------------------\n');
-    }
-    if (diff > max || !max) {
-      max = diff;
-      maxPos = i;
-    }
-  }
-  console.log('max difference is at', maxPos);
-  return maxPos;
-};
-
-
 /*
 * Server Logic
 */
-var https = require('https');
-var fs = require('fs');
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
+var fs = require('fs'),
+express    = require('express'),
+app        = express(),
+bodyParser = require('body-parser'),
+ShotCaller = require('./logic'),
 
-var hskey = fs.readFileSync('https/hacksparrow-key.pem');
-var hscert = fs.readFileSync('https/hacksparrow-cert.pem');
-
-var options = {
-    key: hskey,
-    cert: hscert
+https = require('https'),
+hskey = fs.readFileSync('https/hacksparrow-key.pem'),
+hscert = fs.readFileSync('https/hacksparrow-cert.pem'),
+options = {
+  key: hskey,
+  cert: hscert
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -207,7 +21,6 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 9999;    // set our port
 
 var router = express.Router();        
-
 router.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -229,5 +42,5 @@ router.post('/', function (req, res) {
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
-console.log('Magic happens on port ' + port);
+console.log('Server started on port ' + port);
 https.createServer(options, app).listen(port);
